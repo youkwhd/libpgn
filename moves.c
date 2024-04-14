ac4cce7ae62dbe16e3bfa6471c923ada5fdb4cde
youkwhd <lolywk@tutanota.com>
2024-04-14 15:01:03 +0700

    feat: define array of moves

--- moves.c
+++ moves.c
@@ -1,1 +1,27 @@
 #include "moves.h"
+
+pgn_moves_t *pgn_moves_init()
+{
+    pgn_moves_t *moves = malloc(sizeof *moves);
+    moves->values = malloc(sizeof *moves->values * PGN_MOVES_INITIAL_SIZE);
+    moves->size = PGN_MOVES_INITIAL_SIZE;
+    moves->length = 0;
+
+    return moves;
+}
+
+void pgn_moves_push(pgn_moves_t *moves, pgn_move_t move)
+{
+    if (moves->length >= moves->size) {
+        moves->size += PGN_MOVES_GROW_SIZE;
+        moves->values = realloc(moves->values, moves->size);
+    }
+
+    moves->values[moves->length++] = move;
+}
+
+void pgn_moves_cleanup(pgn_moves_t *moves)
+{
+    free(moves->values);
+    free(moves);
+}
--- moves.h
+++ moves.h
@@ -5,7 +5,11 @@
 #include "coordinate.h"
 #include "annotation.h"
 
+#include <stdlib.h>
 #include <stdbool.h>
+
+#define PGN_MOVES_INITIAL_SIZE 32
+#define PGN_MOVES_GROW_SIZE 32
 
 typedef struct pgn_move_t {
     pgn_piece_t piece;
@@ -13,5 +17,15 @@ typedef struct pgn_move_t {
     pgn_coordinate_t to;
     pgn_annotation_t annotation;
 } pgn_move_t;
+
+typedef struct pgn_moves_t {
+    pgn_move_t *values;
+    size_t length;
+    size_t size;
+} pgn_moves_t;
+
+pgn_moves_t *pgn_moves_init();
+void pgn_moves_push(pgn_moves_t *moves, pgn_move_t move);
+void pgn_moves_cleanup(pgn_moves_t *moves);
 
 #endif // __LIBPGN_MOVES_H
