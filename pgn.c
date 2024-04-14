25eb62fac87f3af9d6fcf502e0a24f8f9e2e92c8
youkwhd <lolywk@tutanota.com>
2024-04-14 14:07:57 +0700

    refactor: move parse metadata to it's own function

--- pgn.c
+++ pgn.c
@@ -16,6 +16,57 @@ void pgn_cleanup(pgn_t *pgn)
     free(pgn);
 }
 
+size_t pgn_parse_metadata(pgn_t *pgn, char *str)
+{
+    unsigned int cursor = 0;
+
+    for (;;) {
+        if (str[cursor] != '[') return cursor;
+        cursor++;
+
+        pgn_string_t *key_buffer = pgn_string_init();
+        pgn_string_t *value_buffer = pgn_string_init();
+
+        while (str[cursor] != ' ') pgn_string_append(key_buffer, str[cursor++]);
+        pgn_string_append_null_terminator(key_buffer);
+
+        assert(str[++cursor] == '"');
+        cursor++;
+
+        while (str[cursor] != '"') pgn_string_append(value_buffer, str[cursor++]);
+        pgn_string_append_null_terminator(value_buffer);
+
+        pgn_table_insert(pgn->metadata, key_buffer->buf, value_buffer->buf);
+        pgn_string_reset(key_buffer);
+        pgn_string_reset(value_buffer);
+
+        assert(str[++cursor] == ']');
+        /* TODO: maybe expect whitespaces than just newline
+         */
+        assert(str[++cursor] == '\n');
+        cursor++;
+
+        pgn_string_cleanup(key_buffer);
+        pgn_string_cleanup(value_buffer);
+    }
+
+    return cursor;
+}
+
+size_t pgn_parse_move(pgn_t *pgn, char *str)
+{
+    PGN_NOT_IMPLEMENTED();
+    unsigned int cursor = 0;
+    return cursor;
+}
+
+size_t pgn_parse_moves(pgn_t *pgn, char *str)
+{
+    PGN_NOT_IMPLEMENTED();
+    unsigned int cursor = 0;
+    return cursor;
+}
+
 void pgn_parse_from_file(pgn_t *pgn, FILE *file)
 {
     PGN_UNUSED(pgn);
@@ -28,39 +79,6 @@ void pgn_parse_from_string(pgn_t *pgn, char *str)
     PGN_UNUSED(pgn);
     PGN_UNUSED(str);
 
-    pgn_string_t *key_buffer = pgn_string_init();
-    pgn_string_t *value_buffer = pgn_string_init();
-
-    int cursor = 0;
-
-__parse_metadata:
-    if (str[cursor] == '[') {
-        cursor++;
-
-        while (str[cursor] != ' ') {
-            pgn_string_append(key_buffer, str[cursor++]);
-        }
-        pgn_string_append_null_terminator(key_buffer);
-
-        assert(str[++cursor] == '"');
-        cursor++;
-
-        while (str[cursor] != '"') {
-            pgn_string_append(value_buffer, str[cursor++]);
-        }
-        pgn_string_append_null_terminator(value_buffer);
-
-        pgn_table_insert(pgn->metadata, key_buffer->buf, value_buffer->buf);
-        pgn_string_reset(key_buffer);
-        pgn_string_reset(value_buffer);
-
-        assert(str[++cursor] == ']');
-        assert(str[++cursor] == '\n');
-        cursor++;
-
-        goto __parse_metadata;
-    }
-
-    pgn_string_cleanup(key_buffer);
-    pgn_string_cleanup(value_buffer);
+    unsigned int cursor = 0;
+    cursor += pgn_parse_metadata(pgn, str);
 }
