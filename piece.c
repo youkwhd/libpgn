c06caa26ea7b45a5182a2927cdc6729134fa811e
youkwhd <lolywk@tutanota.com>
2024-04-14 14:22:07 +0700

    chore: rename knight alphab, add pgn_piece_parse()

--- piece.c
+++ piece.c
@@ -1,1 +1,14 @@
 #include "piece.h"
+
+pgn_piece_t pgn_piece_parse_from_alphabet(char ch)
+{
+    switch (ch) {
+    case PGN_PIECE_PAWN_ALPHABET: return PGN_PIECE_PAWN;
+    case PGN_PIECE_ROOK_ALPHABET: return PGN_PIECE_ROOK;
+    case PGN_PIECE_KNIGHT_ALPHABET: return PGN_PIECE_KNIGHT;
+    case PGN_PIECE_BISHOP_ALPHABET: return PGN_PIECE_BISHOP;
+    case PGN_PIECE_QUEEN_ALPHABET: return PGN_PIECE_QUEEN;
+    case PGN_PIECE_KING_ALPHABET: return PGN_PIECE_KING;
+    default: return PGN_PIECE_PAWN;
+    }
+}
--- piece.h
+++ piece.h
@@ -7,7 +7,7 @@
 
 #define PGN_PIECE_PAWN_ALPHABET 'P'
 #define PGN_PIECE_ROOK_ALPHABET 'R'
-#define PGN_PIECE_KNIGHT_ALPHABET 'K'
+#define PGN_PIECE_KNIGHT_ALPHABET 'N'
 #define PGN_PIECE_BISHOP_ALPHABET 'B'
 #define PGN_PIECE_QUEEN_ALPHABET 'Q'
 #define PGN_PIECE_KING_ALPHABET 'K'
@@ -20,5 +20,7 @@ typedef enum pgn_piece_t {
     PGN_PIECE_QUEEN,
     PGN_PIECE_KING,
 } pgn_piece_t;
+
+pgn_piece_t pgn_piece_parse_from_alphabet(char ch);
 
 #endif // __LIBPGN_PIECE_H
