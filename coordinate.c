b5d6c33baf1d07f313a97eb0b7d132ee9c7c4e20
youkwhd <lolywk@tutanota.com>
2024-04-16 18:09:03 +0700

    chore: rename _number

--- coordinate.c
+++ coordinate.c
@@ -5,7 +5,7 @@ char pgn_coordinate_as_char(int n)
     return n + 'a';
 }
 
-int pgn_coordinate_as_number(char n)
+int pgn_coordinate_as_int(char n)
 {
     return n - 'a';
 }
--- coordinate.h
+++ coordinate.h
@@ -2,10 +2,11 @@
 #define __LIBPGN_COORDINATE_H
 
 typedef struct pgn_coordinate_t {
-    int x, y;
+    char x;
+    int y;
 } pgn_coordinate_t;
 
 char pgn_coordinate_as_char(int n);
-int pgn_coordinate_as_number(char n);
+int pgn_coordinate_as_int(char n);
 
 #endif // __LIBPGN_COORDINATE_H
