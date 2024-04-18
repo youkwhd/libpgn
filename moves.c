380d1316ad84a0a3376cef2fe895791eaf057343
youkwhd <lolywk@tutanota.com>
2024-04-18 19:54:48 +0700

    feat(wip): parse bunch of moves

--- moves.c
+++ moves.c
@@ -71,6 +71,59 @@ pgn_move_t pgn_move_from_string(char *str)
     return __pgn_move_from_string(str, &consumed);
 }
 
+__pgn_moves_t *__pgn_moves_init()
+{
+    __pgn_moves_t *__moves = malloc(sizeof *__moves);
+    // __moves->alternatives = {0};
+    __moves->alternatives.values = malloc(sizeof *__moves->alternatives.values * PGN_MOVES_ALTERNATIVES_INITIAL_SIZE);
+    __moves->alternatives.length = 0;
+    __moves->alternatives.size = PGN_MOVES_ALTERNATIVES_INITIAL_SIZE;
+
+    return __moves;
+}
+
+void __pgn_moves_cleanup(__pgn_moves_t *__moves)
+{
+    free(__moves->alternatives.values);
+    free(__moves);
+}
+
+__pgn_moves_t *__pgn_moves_from_string(char *str, size_t *consumed)
+{
+    size_t cursor = 0;
+    __pgn_moves_t *__moves = __pgn_moves_init();
+
+    if (isdigit(str[cursor]) && str[cursor + 1] == '.') {
+        cursor += 2;
+    }
+    while (str[cursor] == ' ') cursor++;
+
+    __moves->white = __pgn_move_from_string(str + cursor, &cursor);
+
+remove_whitespaces:
+    while (str[cursor] == ' ') cursor++;
+    if (str[cursor] == '{') {
+        while (str[cursor] != '}') cursor++;
+        cursor++;
+        goto remove_whitespaces;
+    }
+
+    if (isdigit(str[cursor]) && str[cursor + 1] == '.') {
+        cursor += 4;
+    }
+    while (str[cursor] == ' ') cursor++;
+
+    __moves->black = __pgn_move_from_string(str + cursor, &cursor);
+
+    return __moves;
+}
+
+__pgn_moves_t *pgn_moves_from_string(char *str)
+{
+    size_t consumed = 0;
+    return __pgn_moves_from_string(str, &consumed);
+}
+
 pgn_moves_t *pgn_moves_init()
 {
     pgn_moves_t *moves = malloc(sizeof *moves);
@@ -81,18 +134,19 @@ pgn_moves_t *pgn_moves_init()
     return moves;
 }
 
-void pgn_moves_push(pgn_moves_t *moves, pgn_move_t move)
+void pgn_moves_push(pgn_moves_t *moves, __pgn_moves_t __moves)
 {
     if (moves->length >= moves->size) {
         moves->size += PGN_MOVES_GROW_SIZE;
         moves->values = realloc(moves->values, moves->size);
     }
 
-    moves->values[moves->length++] = move;
+    moves->values[moves->length++] = __moves;
 }
 
 void pgn_moves_cleanup(pgn_moves_t *moves)
 {
+    free(moves->values->alternatives.values);
     free(moves->values);
     free(moves);
 }
--- moves.h
+++ moves.h
@@ -25,8 +25,20 @@ typedef struct pgn_move_t {
     pgn_annotation_t annotation;
 } pgn_move_t;
 
+#define PGN_MOVES_ALTERNATIVES_INITIAL_SIZE 6
+
+typedef struct __pgn_moves_t {
+    pgn_move_t white;
+    pgn_move_t black;
+    struct {
+        pgn_move_t *values;
+        size_t length;
+        size_t size;
+    } alternatives;
+} __pgn_moves_t;
+
 typedef struct pgn_moves_t {
-    pgn_move_t *values;
+    __pgn_moves_t *values;
     size_t length;
     size_t size;
 } pgn_moves_t;
@@ -34,8 +46,14 @@ typedef struct pgn_moves_t {
 pgn_move_t __pgn_move_from_string(char *str, size_t *consumed);
 pgn_move_t pgn_move_from_string(char *str);
 
-pgn_moves_t *pgn_moves_init();
-void pgn_moves_push(pgn_moves_t *moves, pgn_move_t move);
-void pgn_moves_cleanup(pgn_moves_t *moves);
+__pgn_moves_t *__pgn_moves_from_string(char *str, size_t *consumed);
+__pgn_moves_t *pgn_moves_from_string(char *str);
+
+__pgn_moves_t *__pgn_moves_init();
+void __pgn_moves_cleanup(__pgn_moves_t *__moves);
+
+// pgn_moves_t *pgn_moves_init();
+// void pgn_moves_push(pgn_moves_t *moves, pgn_move_t move);
+// void pgn_moves_cleanup(pgn_moves_t *moves);
 
 #endif // __LIBPGN_MOVES_H
