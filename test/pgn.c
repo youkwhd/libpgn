a5ddeac38bf997554f8a6b5dd819629ecaa04a3c
youkwhd <lolywk@tutanota.com>
2024-04-22 13:20:38 +0700

    chore: add test for a complete pgn file

---
+++ test/pgn.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "../pgn.h"
+#include <assert.h>
+
+unsigned int __nth = 1;
+
+void __print_success()
+{
+    printf("[%d] Assertion success\n", __nth++);
+}
+
+void test_parsing_pgn()
+{
+    printf("calling %s()\n", __func__);
+    __nth = 1;
+
+    pgn_t *pgn = pgn_init();
+    pgn_parse(pgn, "[Event \"Ch City (open)\"]\n[Site \"Frankfurt (Germany)\"]\n\n1.e4 e5\n2.Nc3 Nc6\n3. g3 0-1");
+
+    assert(strcmp(pgn_table_get(pgn->metadata, "Event"), "Ch City (open)") == 0);
+    assert(strcmp(pgn_table_get(pgn->metadata, "Site"), "Frankfurt (Germany)") == 0);
+
+    assert(pgn->moves->values[0]->white.piece == PGN_PIECE_PAWN);
+    assert(pgn->moves->values[0]->white.dest.x == 'e');
+    assert(pgn->moves->values[0]->white.dest.y == 4);
+    assert(pgn->moves->values[0]->white.captures == false);
+    assert(pgn->moves->values[0]->white.annotation == PGN_ANNOTATION_NONE);
+    assert(pgn->moves->values[0]->black.piece == PGN_PIECE_PAWN);
+    assert(pgn->moves->values[0]->black.dest.x == 'e');
+    assert(pgn->moves->values[0]->black.dest.y == 5);
+    assert(pgn->moves->values[0]->black.captures == false);
+    assert(pgn->moves->values[0]->black.annotation == PGN_ANNOTATION_NONE);
+    assert(pgn->moves->values[0]->alternatives == NULL);
+
+    assert(pgn->moves->values[1]->white.piece == PGN_PIECE_KNIGHT);
+    assert(pgn->moves->values[1]->white.dest.x == 'c');
+    assert(pgn->moves->values[1]->white.dest.y == 3);
+    assert(pgn->moves->values[1]->white.captures == false);
+    assert(pgn->moves->values[1]->white.annotation == PGN_ANNOTATION_NONE);
+    assert(pgn->moves->values[1]->black.piece == PGN_PIECE_KNIGHT);
+    assert(pgn->moves->values[1]->black.dest.x == 'c');
+    assert(pgn->moves->values[1]->black.dest.y == 6);
+    assert(pgn->moves->values[1]->black.captures == false);
+    assert(pgn->moves->values[1]->black.annotation == PGN_ANNOTATION_NONE);
+    assert(pgn->moves->values[1]->alternatives == NULL);
+
+    assert(pgn->score.white == 0 && pgn->score.black == 1);
+
+    pgn_cleanup(pgn);
+    __print_success();
+}
+
+int main(void)
+{
+    test_parsing_pgn();
+    return 0;
+}
