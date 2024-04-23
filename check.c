5579dbfec1aed2eb6dac9fd15d19154089927fe8
youkwhd <lolywk@tutanota.com>
2024-04-23 15:52:10 +0700

    chore: add check as it's own type

---
+++ check.c
@@ -0,0 +1,21 @@
+#include "check.h"
+
+pgn_check_t __pgn_check_from_string(char *str, size_t *consumed)
+{
+    int cursor = 0;
+    pgn_check_t check = PGN_CHECK_NONE;
+
+    while (str[cursor] == '+') {
+        check++;
+        cursor++;
+    }
+
+    *consumed += cursor;
+    return check;
+}
+
+pgn_check_t pgn_check_from_string(char *str)
+{
+    size_t consumed = 0;
+    return __pgn_check_from_string(str, &consumed);
+}
---
+++ check.h
@@ -0,0 +1,15 @@
+#ifndef __LIBPGN_CHECK_H
+#define __LIBPGN_CHECK_H
+
+#include <stddef.h>
+
+typedef enum pgn_check_t {
+    PGN_CHECK_NONE = 0,
+    PGN_CHECK_SINGLE,
+    PGN_CHECK_DOUBLE,
+} pgn_check_t;
+
+pgn_check_t __pgn_check_from_string(char *str, size_t *consumed);
+pgn_check_t pgn_check_from_string(char *str);
+
+#endif // __LIBPGN_CHECK_H
