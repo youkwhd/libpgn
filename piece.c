7a83347b45f89d11314dc087331f34fd07b04e0e
youkwhd <lolywk@tutanota.com>
2024-08-01 03:45:00 +0700

    fix: piece wasn't piecing

--- piece.c
+++ piece.c
@@ -1,12 +1,19 @@
 #include "piece.h"
 
+#include <stddef.h>
+
 pgn_piece_t pgn_piece_from_char(char ch)
 {
-    switch ((pgn_piece_t)ch) {
+    switch (ch) {
+    case PGN_PIECE_PAWN:
+    case PGN_PIECE_ROOK:
+    case PGN_PIECE_KNIGHT:
+    case PGN_PIECE_BISHOP:
+    case PGN_PIECE_QUEEN:
+    case PGN_PIECE_KING:
+        return ch;
     default: return PGN_PIECE_UNKNOWN;
     }
-
-    return (pgn_piece_t)ch;
 }
 
 char *pgn_piece_to_string(pgn_piece_t piece)
@@ -18,8 +25,6 @@ char *pgn_piece_to_string(pgn_piece_t piece)
     case PGN_PIECE_BISHOP: return "Bishop";
     case PGN_PIECE_QUEEN: return "Queen";
     case PGN_PIECE_KING: return "King";
-    default: {} // unreachable
+    default: return NULL;
     }
-
-    return 0;
 }
