431b055c8fa49c8b688357e7f76c8193e0e9fb9d
youkwhd <lolywk@tutanota.com>
2024-07-31 18:16:33 +0700

    chore: add tests for piece, delete defines

--- piece.c
+++ piece.c
@@ -2,30 +2,11 @@
 
 pgn_piece_t pgn_piece_from_char(char ch)
 {
-    switch (ch) {
-    case PGN_PIECE_PAWN_CHAR: return PGN_PIECE_PAWN;
-    case PGN_PIECE_ROOK_CHAR: return PGN_PIECE_ROOK;
-    case PGN_PIECE_KNIGHT_CHAR: return PGN_PIECE_KNIGHT;
-    case PGN_PIECE_BISHOP_CHAR: return PGN_PIECE_BISHOP;
-    case PGN_PIECE_QUEEN_CHAR: return PGN_PIECE_QUEEN;
-    case PGN_PIECE_KING_CHAR: return PGN_PIECE_KING;
+    switch ((pgn_piece_t)ch) {
     default: return PGN_PIECE_UNKNOWN;
     }
-}
 
-char pgn_piece_to_char(pgn_piece_t piece)
-{
-    switch (piece) {
-    case PGN_PIECE_PAWN: return PGN_PIECE_PAWN_CHAR;
-    case PGN_PIECE_ROOK: return PGN_PIECE_ROOK_CHAR;
-    case PGN_PIECE_KNIGHT: return PGN_PIECE_KNIGHT_CHAR;
-    case PGN_PIECE_BISHOP: return PGN_PIECE_BISHOP_CHAR;
-    case PGN_PIECE_QUEEN: return PGN_PIECE_QUEEN_CHAR;
-    case PGN_PIECE_KING: return PGN_PIECE_KING_CHAR;
-    default: {} // unreachable
-    }
-
-    return 0;
+    return (pgn_piece_t)ch;
 }
 
 char *pgn_piece_to_string(pgn_piece_t piece)
--- piece.h
+++ piece.h
@@ -3,29 +3,17 @@
 
 #include "export.h"
 
-/* types that represent
- * chess pieces
- */
-
-#define PGN_PIECE_PAWN_CHAR 'P'
-#define PGN_PIECE_ROOK_CHAR 'R'
-#define PGN_PIECE_KNIGHT_CHAR 'N'
-#define PGN_PIECE_BISHOP_CHAR 'B'
-#define PGN_PIECE_QUEEN_CHAR 'Q'
-#define PGN_PIECE_KING_CHAR 'K'
-
 typedef enum pgn_piece_t {
     PGN_PIECE_UNKNOWN = 0,
-    PGN_PIECE_PAWN,
-    PGN_PIECE_ROOK,
-    PGN_PIECE_KNIGHT,
-    PGN_PIECE_BISHOP,
-    PGN_PIECE_QUEEN,
-    PGN_PIECE_KING,
+    PGN_PIECE_PAWN = 'P',
+    PGN_PIECE_ROOK = 'R',
+    PGN_PIECE_KNIGHT = 'N',
+    PGN_PIECE_BISHOP = 'B',
+    PGN_PIECE_QUEEN = 'Q',
+    PGN_PIECE_KING = 'K',
 } pgn_piece_t;
 
 PGN_EXPORT pgn_piece_t pgn_piece_from_char(char ch);
-PGN_EXPORT char pgn_piece_to_char(pgn_piece_t piece);
 PGN_EXPORT char *pgn_piece_to_string(pgn_piece_t piece);
 
 #endif // __LIBPGN_PIECE_H
