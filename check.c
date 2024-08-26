63d52484600be7f1796467273f41f1c08c0f22d2
youkwhd <lolywk@tutanota.com>
2024-08-26 21:11:38 +0700

    chore: cursor should be size_t

--- check.c
+++ check.c
@@ -4,7 +4,7 @@
 
 pgn_check_t __pgn_check_from_string(char *str, size_t *consumed)
 {
-    int cursor = 0;
+    size_t cursor = 0;
     pgn_check_t check = PGN_CHECK_NONE;
 
     while (str[cursor] == '+') {
