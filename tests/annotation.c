894b7e5aa9e7b3a8e6412b69487cfa73c7f050ca
youkwhd <lolywk@tutanota.com>
2024-08-27 22:39:10 +0700

    chore: add more tests

--- tests/annotation.c
+++ tests/annotation.c
@@ -10,6 +10,15 @@ void test_parsing_moves_with_annotation()
     moves = pgn_moves_from_string("1. a4?? a5!");
     suite6_assert(moves->values[0].white.annotation == PGN_ANNOTATION_BLUNDER);
     suite6_assert(moves->values[0].black.annotation == PGN_ANNOTATION_GOOD_MOVE);
+    suite6_assert(!strcmp(moves->values[0].white.notation, "a4??"));
+    suite6_assert(!strcmp(moves->values[0].black.notation, "a5!"));
+    pgn_moves_cleanup(moves);
+
+    moves = pgn_moves_from_string("1. a4!! a5?!");
+    suite6_assert(moves->values[0].white.annotation == PGN_ANNOTATION_BRILLIANT_MOVE);
+    suite6_assert(moves->values[0].black.annotation == PGN_ANNOTATION_DUBIOUS_MOVE);
+    suite6_assert(!strcmp(moves->values[0].white.notation, "a4!!"));
+    suite6_assert(!strcmp(moves->values[0].black.notation, "a5?!"));
     pgn_moves_cleanup(moves);
 }
 
@@ -20,18 +29,23 @@ void test_parsing_moves_with_NAG_annotation()
     moves = pgn_moves_from_string("1. a4 $4 a5 $1");
     suite6_assert(moves->values[0].white.annotation == PGN_ANNOTATION_BLUNDER);
     suite6_assert(moves->values[0].black.annotation == PGN_ANNOTATION_GOOD_MOVE);
+    suite6_assert(!strcmp(moves->values[0].white.notation, "a4 $4"));
+    suite6_assert(!strcmp(moves->values[0].black.notation, "a5 $1"));
     pgn_moves_cleanup(moves);
 
     moves = pgn_moves_from_string("1. a4 $69 a5 $420");
     suite6_assert(moves->values[0].white.annotation == 69);
     suite6_assert(moves->values[0].black.annotation == 420);
+    suite6_assert(!strcmp(moves->values[0].white.notation, "a4 $69"));
+    suite6_assert(!strcmp(moves->values[0].black.notation, "a5 $420"));
     pgn_moves_cleanup(moves);
 
     moves = pgn_moves_from_string("6. Nce2 $2 e5 $0 $19 {}");
     suite6_assert(moves->values[0].white.annotation == PGN_ANNOTATION_MISTAKE);
     suite6_assert(moves->values[0].black.annotation == 19);
+    suite6_assert(!strcmp(moves->values[0].white.notation, "Nce2 $2"));
+    suite6_assert(!strcmp(moves->values[0].black.notation, "e5 $0 $19"));
     pgn_moves_cleanup(moves);
-    
 }
 
 int main(void)
