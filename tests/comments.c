efac7b82316b311fe2fcc94567843ee2d2870e81
youkwhd <lolywk@tutanota.com>
2024-08-28 19:34:34 +0700

    chore: add nested comment test

--- tests/comments.c
+++ tests/comments.c
@@ -31,6 +31,17 @@ void test_parsing_moves_with_comments_multiples(void)
     pgn_moves_cleanup(moves);
 }
 
+void test_parsing_moves_with_comments_nested(void)
+{
+    pgn_moves_t *moves;
+
+    moves = pgn_moves_from_string("1.e4 { a comment {inside a comment} ? } e5");
+    suite6_assert(moves->values[0].white.comments->length == 1);
+    suite6_assert(moves->values[0].white.comments->values[0].position == PGN_COMMENT_POSITION_AFTER_MOVE);
+    suite6_assert(!strcmp(moves->values[0].white.comments->values[0].value->buf, " a comment {inside a comment} ? "));
+    pgn_moves_cleanup(moves);
+}
+
 int main(void)
 {
     test_parsing_moves_with_comments_multiples();
