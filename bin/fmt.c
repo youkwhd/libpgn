9190454088fccc88d2ce9578b9a16cc6405a827c
youkwhd <lolywk@tutanota.com>
2024-08-28 13:09:44 +0700

    feat: print alt moves

--- bin/fmt.c
+++ bin/fmt.c
@@ -1,7 +1,11 @@
 #include <pgn.h>
+#include <pgn/metadata.h>
+#include <pgn/moves.h>
 #include <stdbool.h>
 
 #include "fmt.h"
+
+bool fmt_print_moves(pgn_moves_t *moves, int depth, size_t last_index, bool newline);
 
 void fmt_print_tab(int depth, int width)
 {
@@ -12,30 +16,48 @@ void fmt_print_tab(int depth, int width)
     }
 }
 
-bool fmt_print_metadata(pgn_t *pgn)
+bool fmt_print_metadata(pgn_metadata_t *mt)
 {
     bool printed = false;
 
-    for (size_t i = 0; i < pgn->metadata->length; i++) {
-        printf("[%s \"%s\"]\n", pgn->metadata->items[i].key->buf, pgn->metadata->items[i].value->buf);
+    for (size_t i = 0; i < mt->length; i++) {
+        printf("[%s \"%s\"]\n", mt->items[i].key->buf, mt->items[i].value->buf);
         printed = true;
     }
 
     return printed;
 }
 
-bool fmt_print_moves(pgn_t *pgn, int depth)
+bool fmt_print_alternative_moves(pgn_alternative_moves_t *alt, int depth, size_t last_index)
 {
     bool printed = false;
 
-    /* TODO: recursively print alternatives
-     */
-    for (size_t i = 0; i < pgn->moves->length; i++) {
-        pgn_move_t white = pgn->moves->values[i].white;
-        pgn_move_t black = pgn->moves->values[i].black;
+    for (size_t i = 0; i < alt->length; i++) {
+        fmt_print_tab(depth, 2);
+
+        printf("(");
+        fmt_print_moves(alt->values[i], 0, last_index, false);
+        printf(")\n");
+    }
+
+    return printed;
+}
+
+bool fmt_print_moves(pgn_moves_t *moves, int depth, size_t last_index, bool newline)
+{
+    bool printed = false;
+
+    for (size_t i = 0; i < moves->length; i++) {
+        pgn_move_t white = moves->values[i].white;
+        pgn_move_t black = moves->values[i].black;
 
         fmt_print_tab(depth, 2);
-        printf("%zu. %s %s\n", i + 1, white.notation, black.notation);
+        printf("%zu. %s %s", i + last_index, white.notation, black.notation);
+        if (newline) putchar('\n');
+
+        if (moves->values[i].alternatives)
+            fmt_print_alternative_moves(moves->values[i].alternatives, ++depth, i + last_index);
+
         printed = true;
     }
 
@@ -56,9 +78,9 @@ bool fmt_print_score(pgn_t *pgn)
 
 void fmt_print(pgn_t *pgn)
 {
-    if (fmt_print_metadata(pgn))
+    if (fmt_print_metadata(pgn->metadata))
         printf("\n");
 
-    fmt_print_moves(pgn, 0);
+    fmt_print_moves(pgn->moves, 0, 1, true);
     fmt_print_score(pgn);
 }
