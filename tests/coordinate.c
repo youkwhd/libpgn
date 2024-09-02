02500ee277ce18b2f6de0359480e751f0389e339
youkwhd <lolywk@tutanota.com>
2024-09-02 21:39:20 +0700

    chore: add missing test files

--- tests/coordinate.c
+++ tests/coordinate.c
@@ -9,6 +9,9 @@ void test_convert_coordinate()
     suite6_assert(pgn_coordinate_file_as_index('c') == 2);
     suite6_assert(pgn_coordinate_file_as_index('d') == 3);
     suite6_assert(pgn_coordinate_file_as_index('e') == 4);
+    suite6_assert(pgn_coordinate_file_as_index('f') == 5);
+    suite6_assert(pgn_coordinate_file_as_index('g') == 6);
+    suite6_assert(pgn_coordinate_file_as_index('h') == 7);
 }
 
 int main(void)
