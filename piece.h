699c54b56894c17f3622367dfbd665ec46ef461b
youkwhd <lolywk@tutanota.com>
2024-09-02 19:33:13 +0700

    chore: align pgn_piece_t

--- piece.h
+++ piece.h
@@ -3,12 +3,12 @@
 
 typedef enum pgn_piece_t {
     PGN_PIECE_UNKNOWN = 0,
-    PGN_PIECE_PAWN = 'P',
-    PGN_PIECE_ROOK = 'R',
-    PGN_PIECE_KNIGHT = 'N',
-    PGN_PIECE_BISHOP = 'B',
-    PGN_PIECE_QUEEN = 'Q',
-    PGN_PIECE_KING = 'K',
+    PGN_PIECE_PAWN    = 'P',
+    PGN_PIECE_ROOK    = 'R',
+    PGN_PIECE_KNIGHT  = 'N',
+    PGN_PIECE_BISHOP  = 'B',
+    PGN_PIECE_QUEEN   = 'Q',
+    PGN_PIECE_KING    = 'K',
 } pgn_piece_t;
 
 pgn_piece_t pgn_piece_from_char(char ch);
